7ed7dd3be92f9bdb2c1ff5e51572fa0106d35ded
Екатерина <korobkova.ek99@mail.ru>
2019-11-24 20:36:09 +0300

    task4

---
+++ Korobkova/base/main.cpp
@@ -0,0 +1,26 @@
+﻿#include "cluster.h"
+#include <iostream>
+#include <string>
+
+#define MAXsizeQ  100
+
+using namespace std;
+
+int main()
+{
+	int number_of_processors;
+
+	int tacts;
+
+	cout << " Enter count of processors " << endl;
+	cin >> number_of_processors;
+	cout << endl;
+
+	cout << "Enter count of tacts" << endl;
+	cin >> tacts;
+	cout << endl;
+
+	CLUSTER cluster;
+	cluster.start(tacts, number_of_processors);
+	cluster.statistics();
+}
--- Korobkova/cluster.cpp
+++
@@ -1,3 +0,0 @@
-﻿#include ".h"
-#include ".h"
-
